0d7e889cf575f36d7d5c2cac1a165a8dc3e9ae61
NIlesh-Shivpuje <36780889+NIlesh-Shivpuje@users.noreply.github.com>
2023-06-21 15:45:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,20 @@
-let btn = document.getElementById("submitData").addEventListener("click", getData);
-let sel = document.getElementById("data");
-let setData = 
-
-function setData(res){
-   let table = document.getElementById('table');
-   let li = `<tr><th>Name</th><th>price</th><th>currency</th><th>country_code</th></tr>`;
-   let AllData = [res.name,res.price,res.currency,res.country_code]
-   console.log(AllData);
-   
-    li += `<tr>
-    <td>${res.name}</td>
-    <td>${res.price} </td>
-    <td>${res.currency}</td>
-    <td>${res.country_code}</td>
-  </tr>`;
- 
-    
- table.innerHTML = li;
-
-    return console.log(li);
-}
-
-
-
-async function getData() {
-    let val = sel.value;
-    const url = `https://real-time-finance-data.p.rapidapi.com/search?query=${val}+&language=en`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '544d29983dmsh79966c4370f1bb3p159eeejsn079eb2ff93a9',
-            'X-RapidAPI-Host': 'real-time-finance-data.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console
-     let r = result.data.stock[0];
-      setData(r)
-    } catch (error) {
-        console.error(error);
+function getCheckedCheckboxesFor(checkboxName) {
+    var checkboxes = document.getElementsByClassName('get_checkBxVal'), values = [];
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            values.push(checkboxes[i].value);
+        } 
     }
+    if(values.length){
+        alert(values)
+    }else{
+        alert("please select the images")
 
+    }
+    return values;
 }
+let submit = document.querySelector('#submitBtn');
+submit.addEventListener('click', getCheckedCheckboxesFor);
+ 
+
 
